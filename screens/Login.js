2ef8d4b00427c84ebc67f0650884905f831fc609
AngieLue123 <angiel8686@gmail.com>
2022-01-04 22:31:40 -0800

    Added styling to Login, added fonts and images to assets folder

--- screens/Login.js
+++ screens/Login.js
@@ -1,16 +1,32 @@
 import React, { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { TouchableWithoutFeedback, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import * as Font from "expo-font";
+import { useFonts } from 'expo-font';
+
+Font.loadAsync({
+    GochiHand: require("../assets/fonts/GochiHand-Regular.ttf"),
+    ShareTechMono: require("../assets/fonts/ShareTechMono-Regular.ttf")
+  })
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const ref_input2 = useRef();
+    
+    let [fontsLoaded] = useFonts({
+        'GochiHand': require("../assets/fonts/GochiHand-Regular.ttf"),
+        'ShareTechMono': require("../assets/fonts/ShareTechMono-Regular.ttf")
+      });
+    
 
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <View style={styles.container}>
                 <StatusBar style='auto' />
+                <Text style={styles.back}>
+                    Back 
+                </Text>
                 <Text style={styles.header}>
                     Sign In
                 </Text>
@@ -19,7 +35,7 @@ export default function Login() {
                     <TextInput
                         style={styles.textInput}
                         placeholder="Username or Email"
-                        placeholderTextColor="#003f5c"
+                        placeholderTextColor="#000000"
                         onChangeText={(email) => setEmail(email)}
                         secureTextEntry={false}
                         onSubmitEditing={() => ref_input2.current.focus()}
@@ -30,7 +46,7 @@ export default function Login() {
                     <TextInput
                         style={styles.textInput}
                         placeholder="Password"
-                        placeholderTextColor="#003f5c"
+                        placeholderTextColor="#000000"
                         secureTextEntry
                         onChangeText={(password) => setPassword(password)}
                         ref={ref_input2}
@@ -50,22 +66,36 @@ export default function Login() {
 }
 
 const styles = StyleSheet.create({
+    
+    
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#EE9B00',
         alignItems: 'center',
         justifyContent: 'center',
     },
-
+    back: { //probably need to change the location of the back to work for all screens?
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        fontFamily: 'GochiHand',
+        position: 'absolute',
+        width: 95,
+        height: 20,
+        left: 20,
+        top: 50,
+        fontSize: 24
+    },
     header: {
-        fontSize: 40,
+        fontSize: 48,
         fontWeight: 'bold',
-        marginBottom: 30
+        marginBottom: 30,
+        fontFamily: 'GochiHand'
     },
 
     inputView: {
-        backgroundColor: "#FFC0CB",
-        borderRadius: 30,
+        backgroundColor: "#F8FDF4",
+        borderRadius: 10,
+        borderWidth: 1,
         width: "70%",
         height: 45,
         marginBottom: 20,
@@ -74,26 +104,33 @@ const styles = StyleSheet.create({
 
     textInput: {
         height: 50,
+        fontSize: 20,
         flex: 1,
+        fontFamily: 'ShareTechMono',
+        textDecorationLine: 'underline',
     },
 
     forgot_button: {
         height: 30,
         marginBottom: 0,
+        fontFamily: 'ShareTechMono'
     },
 
     sign_up_button: {
         height: 30,
         marginBottom: 30,
+        fontFamily: 'ShareTechMono'
     },
      
     login_button: {
-        width: "80%",
+        width: "35%",
+        borderWidth: 3,
         borderRadius: 25,
         height: 50,
         alignItems: "center",
         justifyContent: "center",
         marginTop: 40,
-        backgroundColor: "#FF1493",
+        backgroundColor: "#A5A6F6",
+        fontFamily: 'ShareTechMono', //something about touchable opacity is making this hard to change
     },
   });
