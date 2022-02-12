60ea1ad6cd70f2b157f690c8829f8f7c5991323d
jchangz01 <justinklchang@yahoo.com>
2022-02-11 16:24:12 -0800

    Finalized header design and cleaned code

--- screens/Landing.js
+++ screens/Landing.js
@@ -1,24 +1,21 @@
 import React, { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { TouchableWithoutFeedback, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import { TouchableWithoutFeedback, StyleSheet, Text, View, TouchableOpacity, Keyboard } from 'react-native';
 import * as Font from "expo-font";
 import AppLoading from 'expo-app-loading';
 
 
 const fetchFonts = async () => {
-
     return Font.loadAsync({
         ShareTechMono: require("../assets/fonts/ShareTechMono-Regular.ttf"),
         Pacifico: require("../assets/fonts/Pacifico-Regular.ttf"),
       });
 }; 
 
-
 export default function Landing() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const ref_input2 = useRef();
-
 
     const [fontLoaded, setFontLoaded] = useState(false);
     if (!fontLoaded) {
@@ -31,28 +28,16 @@ export default function Landing() {
       );
     }
 
-
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <View style={styles.container}>
                 <StatusBar style='auto' />
                 
-                <View styles = {styles.uppercontainer}>
-                        <Text style={styles.header1}>
-                            Replay
-                        </Text>
-                        
-                        <Text style={styles.header2}>
-                            Replay
-                        </Text>
-                        <Text style={styles.header3}>
-                            Replay
-                        </Text>
-                        <Text style={styles.header4}>
-                            Replay
-                        </Text>
-                    
-
+                <View styles={styles.uppercontainer}>
+                    <Text style={styles.header1}>Replay</Text>
+                    <Text style={styles.header2}>Replay</Text>
+                    <Text style={styles.header3}>Replay</Text>
+                    <Text style={styles.header4}>Replay</Text>
                     <Text style = {styles.slogan}>--slogan--</Text> 
                 </View>
                 
@@ -60,19 +45,13 @@ export default function Landing() {
                 <View style= {styles.lowercontainer}>
                     <Text style = {styles.terms}>By signing up for Replay, you agree to our Terms of Service.</Text>   
                     {/* terms of service will need to probably be a hyperlink to that, needs to be styled? */}
-                
-
                     <TouchableOpacity style={styles.create_account}>
                         <Text style={styles.createText}>Create Account</Text>
                     </TouchableOpacity>
-
-
                     <TouchableOpacity>
                         <Text style={styles.sign_in}>Sign In</Text>
                     </TouchableOpacity>
                 </View>
-                
-
             </View>
         </TouchableWithoutFeedback>
     );
@@ -84,30 +63,27 @@ const styles = StyleSheet.create({
         backgroundColor: '#9B2226',
         alignItems: 'center',
         justifyContent: 'center',
-       flexDirection: 'column',
-       justifyContent: 'space-between'
-        
+        flexDirection: 'column',
+        justifyContent: 'space-between'
     },
+
     uppercontainer: {
         alignItems: 'center',
         position: 'absolute',
-        flex: 1,
+        flex:1,
         justifyContent: 'center',
-        
-        
     },
 
-
+    //farthest bottom header
     header1: {
         fontSize: 64,
         fontWeight: 'bold',
         fontFamily: 'Pacifico',         
         color: '#0A9396',       //blue
         position: 'absolute',
-        top: 135,
+        top: 147,
         left: -100, 
     },
-
     header2: {
         fontSize: 64,
         fontWeight: 'bold',
@@ -115,12 +91,9 @@ const styles = StyleSheet.create({
         color: '#94D2BD',
         margin: 0, 
         position: 'absolute',
-        top: 139,
-        left: -100, 
-       
-        
+        top: 143,
+        left: -96,         
     },
-
     header3: {
         fontSize: 64,
         fontWeight: 'bold',
@@ -128,12 +101,12 @@ const styles = StyleSheet.create({
         color: '#E9D8A6',
         margin: 0, 
         position: 'absolute',
-        top: 143,
-        left: -100, 
+        top: 139,
+        left: -92, 
       
         
     },
-
+    //farthest top header
     header4: {
         fontSize: 64,
         fontWeight: 'bold',
@@ -141,21 +114,16 @@ const styles = StyleSheet.create({
         color: '#EE9B00',       //orange
         margin: 0, 
         position: 'absolute',
-        top: 143,
-        left: -100, 
-        
-       
+        top: 135,
+        left: -88, 
     },
-
-    
 
     lowercontainer: {
         alignItems: 'center',
         position: 'absolute',
         flex: 1,
         justifyContent: 'center',
-        padding: 40, 
-        
+        padding: 40,   
     },
 
     slogan: {
@@ -165,8 +133,6 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 276,
         right: -50,
-        
-
     },
 
     inputView: {
@@ -178,21 +144,6 @@ const styles = StyleSheet.create({
         alignItems: "center",
     },
 
-    textInput: {
-        height: 50,
-        flex: 1,
-    },
-
-    forgot_button: {
-        height: 30,
-        marginBottom: 0,
-    },
-
-    sign_up_button: {
-        height: 30,
-        marginBottom: 30,
-    },
-     
     create_account: {
         width: 295,
         borderRadius: 50,
@@ -220,8 +171,6 @@ const styles = StyleSheet.create({
         fontSize: 24,
         color: '#E9D8A6',
         fontWeight: "bold",
-        
-
     },
 
     createText: {
