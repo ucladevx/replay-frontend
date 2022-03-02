7dd05ae34208abbcb59d374659c899c8680c5d7a
jchangz01 <justinklchang@yahoo.com>
2022-03-01 16:26:00 -0800

    Added user markers and star marker

--- components/UserMarker.js
+++ components/UserMarker.js
@@ -1,23 +1,37 @@
-//////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 // Component: Marker design for each user displayed on map
 //////////////////////////////////////////////////////////
 import React from 'react';
-import { StyleSheet } from 'react-native-web';
+import { View, Image, StyleSheet } from 'react-native';
 import { Marker } from 'react-native-maps';
-
 export default function UserMarker({uri, latitude, longitude}){
     return (
-        <Marker 
+        <Marker
             coordinate={{
                 latitude: latitude,
                 longitude: longitude
             }}
         >
-            {/*<Text>Enter design here</Text>*/}
+            <View style={styles.albumImageContainer}>
+            <Image
+                source={{ 'uri': uri }}
+                style={styles.albumImage}
+            />
+            </View>
         </Marker>
     )
 }
-
 const styles = StyleSheet.create({
-
+    albumImageContainer: {
+        height: 22,
+        width: 22,
+        borderRadius: 11,
+        borderWidth: 4,
+        position: 'relative'
+    },
+    albumImage: {
+        width: 14,
+        height: 14,
+        borderRadius: 14/2,
+    }
 });
